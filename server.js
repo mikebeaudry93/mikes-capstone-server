4abb2ea187c3855787e7ef691cc3f067f2d67893
Mike Beaudry <michaelbeaudry93@gmail.com>
2021-03-05 16:16:27 -0500

    new function

--- server.js
+++ server.js
@@ -15,4 +15,32 @@ app.get("/result", (req, res) => {
   res.send(data);
 });
 
+var http = require("http"); //importing http
+
+function startKeepAlive() {
+  setInterval(function () {
+    var options = {
+      host: "https://ufitness.herokuapp.com/",
+      port: 3001,
+      path: "/",
+    };
+    http
+      .get(options, function (res) {
+        res.on("data", function (chunk) {
+          try {
+            // optional logging... disable after it's working
+            console.log("HEROKU RESPONSE: " + chunk);
+          } catch (err) {
+            console.log(err.message);
+          }
+        });
+      })
+      .on("error", function (err) {
+        console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
 app.listen(process.env.PORT || 3001, () => console.log("server online!"));
