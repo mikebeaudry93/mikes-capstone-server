af14bec82d29553b9c76953adb843ffd2265ab6b
Mike Beaudry <michaelbeaudry93@gmail.com>
2021-03-05 11:05:06 -0500

    final

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // import data
-const data = require("../server/data/data.json");
+const data = require("./data/data.json");
 
 const app = express();
 
@@ -11,38 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 // GET requests
-
 app.get("/result", (req, res) => {
-    res.send(data);
-  });
+  res.send(data);
+});
 
-// app.get("/", (req, res) => {
-//     res.send(data);
-//   });
-
-// app.get("/", (req, res) => {
-//     res.send(data);
-//   });
-
-// app.get("/", (req, res) => {
-//     res.send(data);
-//   });
-
-// app.get("/", (req, res) => {
-//     res.send(data);
-//   });
- 
-
-
-
-  
-  // router.get("/:id", (req, res) => {
-  //   let id = req.params.id;
-  //   const inventoryItem = data.find(item => item.id === id);
-  //   if (!inventoryItem) {
-  //     res.status(404).send("The item with the given ID was not found");
-  //   }
-  //   res.send(inventoryItem);
-  // });
-
-app.listen(8080, () => console.log('server online!'));
+app.listen(process.env.PORT || 3001, () => console.log("server online!"));
